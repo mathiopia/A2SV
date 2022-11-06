b747283451b66169a2930d6aa60d427b3fea9c17
Fitsum <110277170+mathiopia@users.noreply.github.com>
2022-11-06 14:01:45 +0300

    Max Consecutive Ones III

---
+++ max ones.js
@@ -0,0 +1,17 @@
+var longestOnes = function(A, K) {
+   let max = 0
+    for (let left = 0, right = 0; right < A.length; right++) {
+        if (A[right] == 0) {
+            if (K == 0) {
+                while (A[left] == 1) {
+                    left++
+                }
+                left++
+            } else {
+                K--
+            }
+        }
+        max = Math.max(right - left + 1, max)
+    }
+    return max
+}
