f77bf8a05fc3158980dfd135e223db3d830cbfc8
Fitsum <110277170+mathiopia@users.noreply.github.com>
2022-11-11 23:09:38 +0300

    editing some staff

--- sorting senctence.js
+++ sorting senctence.js
@@ -3,20 +3,19 @@ var sortSentence = function(words) {
   let pattern=/\d/g
   let index=words.match(pattern)
   if(index==null){return ''}
-  else{
-  let splited= words.split(" ");
+  let splited= words.split(" ")
   let cpy=[]
   for(let i in splited){
     cpy[index[i]-1]=(splited[i])
   }
-  let result=cpy[0].substr(0, cpy[0].length-1);
+  let result=cpy[0].substr(0, cpy[0].length-1)
   for(let i=1; i<cpy.length ;i++ ){
      result+=" "
      let len= cpy[i].length
-     let newl=cpy[i].substr(0, len-1);
+     let newl=cpy[i].substr(0, len-1)
      result+=newl ;
   }
      return result
            
-  }
+  
 }
